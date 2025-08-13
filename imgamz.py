52d97275d5b4aa25dfc58c21998b3c9b44e03e70
RohanKhodade <rohankhodade883@gmail.com>
2025-08-13 09:33:42 +0530

    output added

--- imgamz.py
+++ imgamz.py
@@ -149,21 +149,18 @@ with tab2:
         st.markdown(output)
 
 
-# ==================== Tab 3: Speaker Info ====================
-with tab3:
-    st.header("🎤 Speaker Info")
-    col1, col2 = st.columns([1, 2])
+# # ==================== Tab 3: Speaker Info ====================
+# with tab3:
+#     st.header("🎤 Speaker Info")
+#     col2 = st.columns([1])
 
-    with col1:
-        st.image("D:\GIGA\First AI Agent\Shreya Dhurde pink Square.jpeg", caption="Speaker Photo")  # Replace with real photo or uploader
-    with col2:
-        st.markdown("""
-        **Name**: Shreya Dhurde  
-        **Role**: AI Engineer
-        **Location**: Pune, India  
-        **Specialties**: Generative AI,AI Agents, LLM Apps  
-        **Talks Given**: 5+ Research & Dev Talks  
+#     with col2:
+#         st.markdown("""
+#         **Name**: Shreya Dhurde  
+#         **Role**: AI Engineer
+#         **Location**: Pune, India  
+#         **Specialties**: Generative AI,AI Agents, LLM Apps  
          
-        """)
+#         """)
 
-        st.markdown("[LinkedIn Profile](https://www.linkedin.com/in/shreya-dhurde/)")  # Replace with real link
+#         st.markdown("[LinkedIn Profile](https://www.linkedin.com/in/Rohan-Khodade/)")  # Replace with real link
